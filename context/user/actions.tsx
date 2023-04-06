6c1caa6750ae0c352fec62dcf69dc59374e32a53
MayurDeopa <frostkickerzvashi@gmail.com>
2023-04-06 11:16:55 +0530

    test 2

--- context/user/actions.tsx
+++ context/user/actions.tsx
@@ -5,25 +5,30 @@ import { uploadImage } from "@/lib/imagekit"
 export const uploadPost =async(dispatch:React.Dispatch<any>,payload:any)=>{
     dispatch({type:'INIT_REQUEST'})
         const {file,post,token} = payload
-        let {imageData,error} = await uploadImage({
-            file:file.file,
-            fileName:file.fileName
-        })
-        if(imageData){
-            let {data,success,err} = await client({url:'post/create',method:'post',payload:{
-                image:{
-                    url:imageData?.url,
-                    height:imageData?.height,
-                    width:imageData?.width
-                },
-                post:{
-                    title:post.title,
-                    caption:post.caption
-                }
-            },token:token})
-            dispatch({type:'FINISH_REQUEST'})
-        }
-        else{
+        try{
+            let {imageData,error} = await uploadImage({
+                file:file.file,
+                fileName:file.fileName
+            })
+            if(imageData){
+                let {data,success,err} = await client({url:'post/create',method:'post',payload:{
+                    image:{
+                        url:imageData?.url,
+                        height:imageData?.height,
+                        width:imageData?.width
+                    },
+                    post:{
+                        title:post.title,
+                        caption:post.caption
+                    }
+                },token:token})
+                dispatch({type:'FINISH_REQUEST'})
+            }
+            else{
+                dispatch({type:'FINISH_REQUEST'})
+            }
+        }catch(err){
+            console.log(err)
             dispatch({type:'FINISH_REQUEST'})
         }
         
