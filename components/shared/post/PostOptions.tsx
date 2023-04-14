72ab977717f7ee7fc4fe91a22adae50ba2e71c41
MayurDeopa <frostkickerzvashi@gmail.com>
2023-04-14 13:47:44 +0530

    err

--- components/shared/post/PostOptions.tsx
+++ components/shared/post/PostOptions.tsx
@@ -9,17 +9,7 @@ interface PostOptionsProps{
     onClick?:()=>void
 }
 
-const options =[
-    {
-        title:"Edit"
-    },
-    {
-        title:'Report',
-    },
-    {
-        title:"Share",
-    }
-]
+
 
 
 const PostOptions:React.FC<PostOptionsProps> =({open,onClick})=>{
@@ -33,7 +23,6 @@ const PostOptions:React.FC<PostOptionsProps> =({open,onClick})=>{
         <React.Fragment>
             <Button.Secondary text=':' action={()=>setShow(!show)} styles={{position:'absolute',right:'10px',top:'10px',cursor:'pointer',fontWeight:'bolder',height:'1.4rem',padding:'var(--space-2)'}}/>
                      
-            <Dropdown open={show}  items={options} style={{position:'absolute',right:'10px',top:'30px'}}/>
             
         </React.Fragment>
         
